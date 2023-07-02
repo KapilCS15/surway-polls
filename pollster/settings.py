6348a3ffdb075d34a8f2233eec4c76df815d4e46
Kapil Saxena <saxenak737@gmail.com>
2023-07-02 23:07:55 +0530

    Deploy

--- pollster/settings.py
+++ pollster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^_&3^e5&d&^1r)7q21ku7kif0czi07fiw_1^&ref^k!y#_h-d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['surway-polls.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
